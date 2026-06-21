a5efefdee0c97418bb587baae875d37266420f96
fahim2023 <fa733m4d4m@outlook.com>
2026-06-21 19:13:59 +0100

    Connect PostgreSQL database via dj-database-url

--- study_market/studymarket/settings.py
+++ study_market/studymarket/settings.py
@@ -17,6 +17,7 @@ if os.path.isfile("env.py"):
     import env
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,10 @@ WSGI_APPLICATION = "studymarket.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
 
 
