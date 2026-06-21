6fde37ee9b0b0898918d830b58100cf67a604d49
fahim2023 <fa733m4d4m@outlook.com>
2026-06-21 22:38:16 +0100

    Add login.html  template

--- studymarket/settings.py
+++ studymarket/settings.py
@@ -145,3 +145,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
