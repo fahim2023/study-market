bfb761e5685fbb37105c8d193b5c397b6223dccf
fahim2023 <fa733m4d4m@outlook.com>
2026-06-22 11:50:36 +0100

    Register Document and DocumentTag in Django admin

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Document, DocumentTag
+
+
+@admin.register(DocumentTag)
+class DocumentTagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("title", "seller", "course", "price", "status", "created_at")
+    list_filter = ("status", "course__subject", "course")
+    search_fields = ("title", "seller__username")
+    prepopulated_fields = {"slug": ("title",)}
+    filter_horizontal = ("tags",)
