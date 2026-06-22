2ef8637a6cc42e4c66ec7dccf066e27d380cd12b
fahim2023 <fa733m4d4m@outlook.com>
2026-06-22 01:19:15 +0100

    Register Subject and Course in Django admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Subject, Course
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("name", "subject", "level", "exam_board")
+    list_filter = ("subject", "level")
+    search_fields = ("name", "subject__name")
+    prepopulated_fields = {"slug": ("name",)}
