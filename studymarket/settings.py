36a9b6fc8f4f5529258018c64dabc8c288660ad5
fahim2023 <fa733m4d4m@outlook.com>
2026-06-25 20:43:09 +0100

    Add home app, URLs and template directory

--- studymarket/settings.py
+++ studymarket/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "documents",
     "payments",
     "reviews",
+    "home",
 ]
 
 STORAGES = {
--- studymarket/urls.py
+++ studymarket/urls.py
@@ -24,7 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("", include("documents.urls")),
+    path("documents/", include("documents.urls")),
     path("payments/", include("payments.urls")),
     path("reviews/", include("reviews.urls")),
+    path("", include("home.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
