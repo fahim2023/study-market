f7535bd4895ca0403753e598eb562c2810788e03
fahim2023 <fa733m4d4m@outlook.com>
2026-06-29 17:10:54 +0100

    Login and register page redesign, brown button, remove password help text

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,3 +9,8 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.help_text = None
